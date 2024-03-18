ddbfbc83a88cd34f76e903396323874e9c833cb4
Akshay Jha <101502244+hey-ak@users.noreply.github.com>
2024-03-18 11:18:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(bodyParser.json());
 
 app.post('/bfhl', (req, res) => {
-const arr = req.body.data;
-console.log(arr);
+    const inputArray = req.body.array;
 
-const user = "akshayjha_2110990127";
+    const evenNumbers = inputArray.filter(num => num % 2 === 0);
+    const oddNumbers = inputArray.filter(num => num % 2 !== 0);
+    const alphabetsUpperCase = inputArray.filter(char => /[a-zA-Z]/.test(char)).map(char => char.toUpperCase());
 
-const alpha = arr.filter(ch => /[a-zA-Z]/.test(ch)).map(ch => ch.toUpperCase());
-const oddArr = arr.filter(item => item % 2 !== 0);
-const evenArr = arr.filter(item => item % 2 === 0);
+    const user_id = "john_doe_17091999";
 
+    const response = {
+        user_id: user_id,
+        is_success: true,
+        even_numbers: evenNumbers,
+        odd_numbers: oddNumbers,
+        alphabets_uppercase: alphabetsUpperCase
+    };
 
-
-const resp = {
-    user_id: user,
-    is_success: true,
-    even_numbers: evenArr,
-    odd_numbers: oddArr,
-    alphabets_uppercase: alpha
-};
-
-res.json(resp);
+    res.json(response);
 });
 
 app.listen(port, () => {
-console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
